4dd327f09cd67e4ee55c0f7e3b23cdf48d439ed7
Hidenori Miyoshi <miyohide@gmail.com>
2013-08-14 23:43:54 +0900

    Add Contact page.

--- test/integration/static_pages_test.rb
+++ test/integration/static_pages_test.rb
@@ -37,4 +37,16 @@ describe "StaticPages Integration Test" do
       page.must_have_title 'Ruby on Rails Tutorial Sample App | About Us'
     end
   end
+
+  describe "Contact page" do
+    it "should have the content 'Contact'" do
+      visit '/static_pages/contact'
+      page.must_have_content('Contact')
+    end
+
+    it "should have the title 'Contact'" do
+      visit '/static_pages/contact'
+      page.must_have_title("Ruby on Rails Tutorial Sample App | Contact")
+    end
+  end
 end
