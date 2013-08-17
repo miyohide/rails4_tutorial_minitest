8b9427b5685acc5836e718200ea4127a563a1d8b
Hidenori Miyoshi <miyohide@gmail.com>
2013-08-17 22:59:19 +0900

    User model test write ( pending )

--- test/models/user_test.rb
+++ test/models/user_test.rb
@@ -1,11 +1,17 @@
+# coding: utf-8
+
 require "test_helper"
 
 describe User do
-  before do
-    @user = User.new
+  it "pending test" do
+    skip "add some examples to ( or delete) #{__FILE__}"
   end
 
-  it "must be valid" do
-    @user.valid?.must_equal true
-  end
+  # before do
+  #   @user = User.new
+  # end
+
+  # it "must be valid" do
+  #   @user.valid?.must_equal true
+  # end
 end
