77dbf22eb52172bef05310b0f7e9adf387ea0e99
Hidenori Miyoshi <miyohide@gmail.com>
2013-08-21 22:46:08 +0900

    Implement for secure passwords.

--- test/models/user_test.rb
+++ test/models/user_test.rb
@@ -15,6 +15,7 @@ describe User do
    it { @user.must_respond_to(:password_digest) }
    it { @user.must_respond_to(:password) }
    it { @user.must_respond_to(:password_confirmation) }
+   it { @user.must_respond_to(:authenticate) }
 
    it { @user.valid?.must_equal true }
 
@@ -64,5 +65,27 @@ describe User do
 
       it { @user.valid?.must_equal false }
    end
+
+   describe "with a password that's too short" do
+      before { @user.password = @user.password_confirmation = "a" * 5 }
+      it { @user.valid?.must_equal false }
+   end
+
+   describe "return value of authenticate method" do
+      before { @user.save }
+      let(:found_user) { User.find_by(email: @user.email) }
+
+      describe "with valid password" do
+         it { @user.must_equal found_user.authenticate(@user.password) }
+      end
+
+      describe "with invalid password" do
+         let(:user_for_invalid_password) { found_user.authenticate("invalid") }
+
+         it { @user.wont_equal user_for_invalid_password }
+         it { user_for_invalid_password.must_equal false }
+      end
+   end
+
 end
 
