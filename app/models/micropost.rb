c5a9fbc502f472befe3036b168b32cd0edfd6d7d
Hidenori Miyoshi <miyohide@gmail.com>
2013-09-01 22:51:44 +0900

    A validation for the micropost's user_id.

--- app/models/micropost.rb
+++ app/models/micropost.rb
@@ -1,2 +1,5 @@
+# coding: utf-8
+
 class Micropost < ActiveRecord::Base
+   validates :user_id, presence: true
 end
--- test/models/micropost_test.rb
+++ test/models/micropost_test.rb
@@ -12,4 +12,11 @@ describe Micropost do
    it { @micropost.must_respond_to(:content) }
    it { @micropost.must_respond_to(:user_id) }
 
+   it { @micropost.must_be :valid? }
+
+   describe "when user_id is not present" do
+      before { @micropost.user_id = nil }
+      it { @micropost.wont_be :valid? }
+   end
+
 end
